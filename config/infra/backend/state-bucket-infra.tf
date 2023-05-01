14a070128e842b478b6d135dd2ee51c699fac6ba
Luke <lukemacdonald21@gmail.com>
2023-05-02 00:08:01 +1000

    updated s3 state bucket backend

--- config/infra/backend/state-bucket-infra.tf
+++ config/infra/backend/state-bucket-infra.tf
@@ -3,16 +3,18 @@ terraform {
 }
 
 resource "aws_s3_bucket" "state_bucket" {
-  bucket = "somestatebucket"
+  bucket = "s3888490-a2-backend"
   acl    = "private"
-
-  versioning {
-    enabled = true
+}
+resource "aws_s3_bucket_versioning" "state_bucket_versioning" {
+  bucket= aws_s3_bucket.state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
 resource "aws_dynamodb_table" "state_bucket_lock" {
-  name           = "somestatelock"
+  name           = "s3888490-a2-backend-db"
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "LockID"
