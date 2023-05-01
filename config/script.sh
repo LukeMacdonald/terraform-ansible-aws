7e2adb488a2656ac5e1f5795a1ad41a8c0294cf6
Luke <lukemacdonald21@gmail.com>
2023-05-02 00:08:25 +1000

    updated script.sh

--- config/script.sh
+++ config/script.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-cd infra || exit
-terraform init
-terraform plan
-terraform apply -auto-approve
+
+TERRAFORM_BACKEND_DIR="infra/backend"
+TERRAFORM_MAIN_DIR=".."
+
+apply_terraform() {
+    terraform init
+    terraform plan
+    terraform apply -auto-approve
+}
+
+# Check that Terraform is installed
+if ! command -v terraform &> /dev/null
+then
+    echo "Error: Terraform not found"
+    exit 1
+fi
+
+# Apply backend configuration
+cd "$TERRAFORM_BACKEND_DIR" || exit
+
+if terraform state show aws_s3_bucket.state_bucket &> /dev/null; then
+  echo "State Bucket already exists. Skipping apply."
+else
+   apply_terraform
+fi
+
+# Apply main configuration
+cd "$TERRAFORM_MAIN_DIR" || exit
+apply_terraform
